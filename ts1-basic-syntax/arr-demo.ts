5f29c4df9f5623a2ea96d69c8aff38dc64c82209
noodles <1205380933@qq.com>
2022-04-13 21:37:15 +0800

    feat: ts1-基本语法之数组&元组

---
+++ ts1-basic-syntax/arr-demo.ts
@@ -0,0 +1,20 @@
+// note: 数组
+const numberArr: (number | string)[] = [1, 2, 3];
+
+const objArr: {name: string}[] = [{
+  name: 'noodles'
+}]
+
+type User = { name: string }
+const objectArr: User[] = [{
+  name: 'noodles'
+}]
+
+class Teacher { name: string }
+
+const objectArr1: Teacher[] = [
+  new Teacher(),
+  {
+    name: 'noodles'
+  }
+]
---
+++ ts1-basic-syntax/tuple-demo.ts
@@ -0,0 +1,8 @@
+// note: 元组
+const teacherInfo: [string, string, number] = ['Dell', 'male', 18];
+// note: 处理csv文件
+const teacherList: [string, string, number][] = [
+  ['name1', 'male', 19],
+  ['name2', 'female', 19],
+  ['name3', 'female', 19],
+];
